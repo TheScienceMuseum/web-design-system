d4ccf713fec7f61b2cbcc23633ca350ddad83546
Toby Stokes <toby@tobystokes.uk>
2019-03-25 18:17:11 +0000

    add components

--- src/components/button/button.config.js
+++ src/components/button/button.config.js
@@ -1,10 +1,19 @@
 module.exports = {
   title: "Buttons",
   status: "wip",
+  collated: true, // show all variants on one page
   context: {
-    label: "Gizza button!"
+    label: "Gizza button!",
+    theme: "blue"
   },
   variants: [
+    {
+      name: "Themed",
+      context: {
+        label: "Purple button!",
+        theme: "purple"
+      }
+    },
     {
       name: "primary",
       context: {
---
+++ src/components/card/card.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  griditem: "top level",
+  label: "Card",
+  status: "wip",
+  preview: "@preview-grid",
+  // display: {
+  //   "max-width": "640px",
+  //   "min-width": "320px"
+  // },
+  context: {
+    griditem: "4-col"
+  }
+};
