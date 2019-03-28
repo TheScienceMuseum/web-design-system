b7e658ee33c046be85734fd05c8c75bae7f1023c
Toby Stokes <toby@tobystokes.uk>
2019-03-28 15:58:14 +0000

    add mastehead cpmponent

---
+++ src/components/page/01-masthead/masthead.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  status: "wip",
+  preview: "@preview-raw",
+  notes: "[todo: define linking rules]",
+  context: {
+    title: "Science Museum Group",
+    logo:
+      "https://learning-resources.sciencemuseum.org.uk/wp-content/themes/smgwptheme/assets/img/smg-logo.svg",
+    link: "https://learning-resources.sciencemuseum.org.uk"
+  }
+};
