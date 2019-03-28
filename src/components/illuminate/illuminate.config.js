cc99baf786735913ab4408f39cd1e331f5a3fe61
Toby Stokes <toby@tobystokes.uk>
2019-03-28 15:56:57 +0000

    illuminate component. my best guess at api for using.

---
+++ src/components/illuminate/illuminate.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  title: "Illuminate",
+  status: "wip",
+  notes: "[todo: clarify limited use cases]",
+  collated: true, // show all variants on one page
+  context: {
+    text: "ILLUMINATED TEXT",
+    class: "js-illuminate"
+  },
+  variants: [
+    {
+      name: "Custom selector",
+      context: {
+        text: "Using a custom selector",
+        class: "custom-class",
+        init:
+          "<script>document.addEventListener('DOMContentLoaded', function(event) {smg.illuminate({selector: '.custom-class'})})</script>"
+      }
+    }
+  ]
+};
--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import accordions from "./components/accordion/accordion.js";
+import illuminate from "./components/illuminate/illuminate.js";
 // console.log("in main");
 document.addEventListener("DOMContentLoaded", function(event) {
   accordions();
+  illuminate();
 });
+
+// expose as globals for other forms of consumption, under smg object.
+window.smg = {};
+window.smg.accordions = accordions;
+window.smg.illuminate = illuminate;
