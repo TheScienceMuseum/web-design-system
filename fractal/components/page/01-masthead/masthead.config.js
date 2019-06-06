b5d6d8e872bf748f671328ae7f31b668c6abbd02
Toby Stokes <toby@tobystokes.uk>
2019-06-06 20:49:56 +0100

    make masthead use our own sprite

--- fractal/components/page/01-masthead/masthead.config.js
+++ fractal/components/page/01-masthead/masthead.config.js
@@ -4,8 +4,7 @@ module.exports = {
   notes: "[todo: define linking rules]",
   context: {
     title: "Science Museum Group",
-    logo:
-      "https://learning-resources.sciencemuseum.org.uk/wp-content/themes/smgwptheme/assets/img/smg-logo.svg",
-    link: "https://learning-resources.sciencemuseum.org.uk"
+    logo: "/assets/logos/sprite.symbol.svg#smg",
+    link: "https://www.sciencemuseum.org.uk"
   }
 };
