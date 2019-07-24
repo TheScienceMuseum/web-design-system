6cebc6165777de8eeaa807ec271ff066d765fb74
Toby Stokes <toby@tobystokes.uk>
2019-07-24 16:26:34 +0100

    collate as variants

--- fractal/components/panel/panel.config.js
+++ fractal/components/panel/panel.config.js
@@ -1,5 +1,18 @@
 module.exports = {
+  collated: true, // show all variants on one page
+
   context: {
     html: "<h2>A panel</h2><p>Various html elements might go in here</p>"
-  }
+  },
+  variants: [
+    {
+      name: "lighter",
+      notes: "Stacked panels get lighter.",
+      context: { html: "a subsequent lighter panel", class: "c-panel--lighter" }
+    },
+    {
+      name: "evenlighter",
+      context: { html: "a subsequent lighter panel", class: "c-panel--lighter" }
+    }
+  ]
 };
