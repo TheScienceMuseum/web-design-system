18d8c32e3a6dac46075dfffd5fcdd08230e22f66
Toby Stokes <toby@tobystokes.uk>
2019-07-26 15:43:52 +0100

    add a11y colour info

--- fractal/docs/03-colour/colours.config.js
+++ fractal/docs/03-colour/colours.config.js
@@ -1,35 +1,58 @@
+var colorable = require("colorable");
+// sadly, these design tokens are repeated values from the scss map. a job for https://github.com/salesforce-ux/theo ?
+var palettes = {
+  red: {
+    name: "$c-red",
+    hex: "#e60060",
+    text: "white"
+  },
+  orange: {
+    name: "$c-orange",
+    hex: "#ec6408",
+    text: "white"
+  },
+  yellow: {
+    name: "$c-yellow",
+    hex: "#ffee00",
+    text: "black"
+  },
+  green: {
+    name: "$c-green",
+    hex: "#95c11f",
+    text: "black"
+  },
+  teal: {
+    name: "$c-teal",
+    hex: "#4bbecf",
+    text: "black"
+  },
+  blue: {
+    name: "$c-blue",
+    hex: "#004899",
+    text: "white"
+  },
+  purple: {
+    name: "$c-purple",
+    hex: "#af1280",
+    text: "white"
+  }
+};
+
+var colorableOptions = {
+  compact: true,
+  threshold: 0
+};
+
+for (var palette in palettes) {
+  colorablePalette = [palettes[palette].hex, palettes[palette].text];
+  var colorableResult = colorable(colorablePalette, colorableOptions);
+  palettes[palette].colorable = colorableResult[0].combinations[0];
+}
+// console.log(palettes.purple.colorable);
+
 module.exports = {
   context: {
-    palettes: {
-      red: {
-        name: "$c-red",
-        hex: "#e60060"
-      },
-      orange: {
-        name: "$c-orange",
-        hex: "#ec6408"
-      },
-      yellow: {
-        name: "$c-yellow",
-        hex: "#ffee00"
-      },
-      green: {
-        name: "$c-green",
-        hex: "#95c11f"
-      },
-      teal: {
-        name: "$c-teal",
-        hex: "#4bbecf"
-      },
-      blue: {
-        name: "$c-blue",
-        hex: "#004899"
-      },
-      purple: {
-        name: "$c-purple",
-        hex: "#af1280"
-      }
-    },
+    palettes: palettes,
     greys: {
       black: {
         name: "Black",
--- fractal/docs/03-colour/colours.md
+++ fractal/docs/03-colour/colours.md
@@ -29,10 +29,43 @@ you can use the `contrasting-text-color` scss function to return either white or
               <dd>{{ name }}</dd>
               <dt>Hex</dt>
               <dd>{{ hex }}</dd>
-              <dt>Lighter</dt>
-              <dd class="u-bg-{{ @key }}-light">For e.g. hover variation</dd>
-              <dt>Darker</dt>
-              <dd class="u-bg-{{ @key }}-dark">For e.g. hover variation</dd>
+           </dl>
+          </div>
+      </div>
+    </article>
+
+  </div>
+
+{{/each}}
+
+</div>
+
+<div class="o-grid o-grid--4-col">
+{{#each palettes }}
+  <div class="o-grid__item">
+    <article class="c-card c-card--default">
+      <div class="c-card__content">
+          <div class="c-card__info">
+              <h3 class="c-card__title">
+                  SMG-{{ @key }}
+              </h3>
+          </div>
+          <div class="c-card__info">
+            <dl class="o-dl">
+             <dt>Contrasting text colour</dt>
+              <dd class="u-bg-{{ @key }}">{{ text }}</dd>
+             <dt>Contrast</dt>
+              <dd>{{ colorable.contrast }}</dd>
+            <dt>accessibility</dt>
+              <dd>
+              {{#each colorable.accessibility}}
+                {{@key}}: {{#if this}}PASS{{else}}FAIL{{/if}}<br/>
+              {{/each}}
+              </dd>
+              <dt>Lighter For e.g. hover variation</dt>
+              <dd class="u-bg-{{ @key }}-light">&nbsp;</dd>
+              <dt>Darker For e.g. hover variation</dt>
+              <dd class="u-bg-{{ @key }}-dark">&nbsp;</dd>
             </dl>
           </div>
       </div>
