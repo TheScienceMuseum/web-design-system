75e50bbf26947077f651a7812391736eef63ccf8
Toby Stokes <toby@tobystokes.uk>
2019-10-21 15:46:16 +0200

    add light panel variant

--- fractal/components/panel/panel.config.js
+++ fractal/components/panel/panel.config.js
@@ -2,7 +2,8 @@ module.exports = {
   collated: true, // show all variants on one page
 
   context: {
-    html: "<h2>A panel</h2><p>Various html elements might go in here</p>"
+    html:
+      "<h2>A panel</h2><p>Generally used in the sidebar</p><p>Various html elements might go in here</p>"
   },
   variants: [
     {
@@ -13,6 +14,14 @@ module.exports = {
     {
       name: "evenlighter",
       context: { html: "a subsequent lighter panel", class: "c-panel--lighter" }
+    },
+    {
+      name: "lightest",
+      context: {
+        html:
+          "A panel with a light background. This might be used in main page to highlight an area of content.",
+        class: "c-panel--light"
+      }
     }
   ]
 };
