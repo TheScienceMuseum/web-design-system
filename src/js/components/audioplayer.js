3911a57dbe689906c9c877f1c523a3f6fd14060a
Toby Stokes <toby@tobystokes.uk>
2019-10-22 12:00:23 +0200

    add audioplayer button label

--- src/js/components/audioplayer.js
+++ src/js/components/audioplayer.js
@@ -3,17 +3,20 @@ import Plyr from "plyr/dist/plyr.polyfilled.js";
 export default function audioplayer() {
   var player = new Plyr(".c-audioplayer audio");
   var stateClass = document.querySelector(".c-audioplayer").classList;
+  var button = document.querySelector(".c-audioplayer__button");
   player.on("playing", event => {
     stateClass.add("c-audioplayer--playing");
+    button.setAttribute("aria-label", "Pause audio");
   });
   player.on("pause", event => {
     stateClass.remove("c-audioplayer--playing");
+    button.setAttribute("aria-label", "Play audio");
   });
   player.on("ended", event => {
     stateClass.remove("c-audioplayer--playing");
+    button.setAttribute("aria-label", "Play audio");
   });
 
-  var button = document.querySelector(".c-audioplayer__button");
   button.addEventListener("click", function(e) {
     player.togglePlay();
   });
