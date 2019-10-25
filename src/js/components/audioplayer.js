a86b9dd06838fc089f11ca72aa5b953940f4ebd3
Toby Stokes <toby@tobystokes.uk>
2019-10-25 15:58:02 +0200

    check for audioplayer before loading

--- src/js/components/audioplayer.js
+++ src/js/components/audioplayer.js
@@ -1,23 +1,25 @@
 import Plyr from "plyr/dist/plyr.polyfilled.js";
 
 export default function audioplayer() {
-  var player = new Plyr(".c-audioplayer audio");
-  var stateClass = document.querySelector(".c-audioplayer").classList;
-  var button = document.querySelector(".c-audioplayer__button");
-  player.on("playing", event => {
-    stateClass.add("c-audioplayer--playing");
-    button.setAttribute("aria-label", "Pause audio");
-  });
-  player.on("pause", event => {
-    stateClass.remove("c-audioplayer--playing");
-    button.setAttribute("aria-label", "Play audio");
-  });
-  player.on("ended", event => {
-    stateClass.remove("c-audioplayer--playing");
-    button.setAttribute("aria-label", "Play audio");
-  });
+  var audioplayer = document.querySelector(".c-audioplayer");
+  if (audioplayer !== null) {
+    var player = new Plyr(".c-audioplayer audio");
+    var button = document.querySelector(".c-audioplayer__button");
+    player.on("playing", event => {
+      audioplayer.classList.add("c-audioplayer--playing");
+      button.setAttribute("aria-label", "Pause audio");
+    });
+    player.on("pause", event => {
+      audioplayer.classList.remove("c-audioplayer--playing");
+      button.setAttribute("aria-label", "Play audio");
+    });
+    player.on("ended", event => {
+      audioplayer.classList.remove("c-audioplayer--playing");
+      button.setAttribute("aria-label", "Play audio");
+    });
 
-  button.addEventListener("click", function(e) {
-    player.togglePlay();
-  });
+    button.addEventListener("click", event => {
+      player.togglePlay();
+    });
+  }
 }
