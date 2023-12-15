64ba1d1b5ee158b6f18b4880dcf12f29fb626121
Toby Stokes <toby@tobystokes.uk>
2023-12-15 19:59:19 +0000

    deploy

---
+++ js/main.js
@@ -0,0 +1,1 @@
+!function(e,t){"object"==typeof exports&&"object"==typeof module?module.exports=t():"function"==typeof define&&define.amd?define([],t):"object"==typeof exports?exports.smgwds=t():e.smgwds=t()}(self,(function(){return function(){"use strict";var e={};return e.default}()}));
